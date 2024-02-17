9c848b44337b7d4ecca77caafe67f43f6570804e
barisAkgul <barisakgul1994@gmail.com>
2024-02-18 02:52:39 +0300

    Added Sign in page template

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,15 @@
+import { redirect } from "next/navigation";
+import React from "react";
+
+const SignInPage = async () => {
+  return (
+    <div className="flex w-full h-screen">
+      <div className="w-full flex justify-center items-center ">
+        {/* Form */}
+        Sign-in Form
+      </div>
+    </div>
+  );
+};
+
+export default SignInPage;
