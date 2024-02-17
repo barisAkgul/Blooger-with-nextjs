3b7bdd4ffc93c6e89dd2909aa3da4fb457e93735
barisAkgul <barisakgul1994@gmail.com>
2024-02-18 01:59:11 +0300

    Added header scrool animation

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { AiOutlineMenu } from "react-icons/ai";
 
@@ -7,9 +8,40 @@ import { Button } from "../ui/button";
 import { MainNav } from "@/components/header/MainNav";
 import { ThemeToggle } from "@/components/header/theme-toggle";
 
-const Header = async () => {
+const Header = () => {
+  const [isHeaderActive, setHeaderActive] = useState(false);
+  const [isBackTopBtnActive, setBackTopBtnActive] = useState(false);
+  const [isMobileNavbarActive, setMobileNavbarActive] = useState(false);
+
+  const toggleMobileNavbar = () => {
+    setMobileNavbarActive(!isMobileNavbarActive);
+    document.body.classList.toggle("nav-active");
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setHeaderActive(true);
+        setBackTopBtnActive(true);
+      } else {
+        setHeaderActive(false);
+        setBackTopBtnActive(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <header className="fixed top-0 left-0 w-full p-6 bg-bg-oxford-blue-2 z-50">
+    <header
+      className={`fixed top-0 left-0 w-full p-6 bg-oxford-blue z-50 ${
+        isHeaderActive ? "header-active" : ""
+      }`}
+      data-header
+    >
       <div className="container flex justify-between items-center gap-10">
         <div>Blooger</div>
         <MainNav className="mx-6 hidden lg:flex" />
@@ -17,7 +49,7 @@ const Header = async () => {
         <div className="flex flex-row gap-10">
           <ThemeToggle />
 
-          <Button size="sm">
+          <Button size="sm" className="btn">
             <Link href="/sign-in">Sign in</Link>
           </Button>
           <button
