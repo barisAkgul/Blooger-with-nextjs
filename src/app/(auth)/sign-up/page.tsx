93ff08371468604adfd299cc7df9d2c4a3ded670
barisAkgul <barisakgul1994@gmail.com>
2024-02-19 01:04:51 +0300

    Sign up page template

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// import SignInForm from "@/components/auth/SignInForm";
+
+const SignInPage = async () => {
+  return (
+    <div className="flex w-full h-screen justify-center">
+      <div className="w-2/3 flex justify-center items-center ">
+        {/* Form */}
+        Sign up Form
+      </div>
+    </div>
+  );
+};
+
+export default SignInPage;
