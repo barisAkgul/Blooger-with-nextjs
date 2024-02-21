923002a6a011d89a90b01647657834dcfc41e939
barisAkgul <barisakgul1994@gmail.com>
2024-02-22 00:19:30 +0300

    Added user posts link

--- src/helpers/config/site.ts
+++ src/helpers/config/site.ts
@@ -2,6 +2,7 @@ export enum ScreenRoutes {
   HOME = "/",
   DASHBOARD = "/dashboard",
   CATEGORY_DASHBOARD = "/dashboard/categories",
+  USER_POSTS = "my-posts",
   POSTS = "/posts",
   NOT_FOUND = "*",
 }
