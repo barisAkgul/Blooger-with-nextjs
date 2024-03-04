613aacf4fb776d9fa4ae6d0936e7a930631b2ff8
barisAkgul <barisakgul1994@gmail.com>
2024-03-04 23:18:00 +0300

    Added components

--- src/components/ui/heading.tsx
+++ src/components/ui/heading.tsx
@@ -31,7 +31,7 @@ const DashboardHeading: React.FC<{ title: string; description: string }> = ({
 }) => {
   return (
     <div>
-      <h2 className="text-3xl font-bold tracking-tight">{title}</h2>
+      <h2 className="text-xl md:text-3xl font-bold tracking-tight">{title}</h2>
       <p className="text-sm text-muted-foreground">{description}</p>
     </div>
   );
