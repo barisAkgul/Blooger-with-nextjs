70f26f6efa6160698a8050f2524ef74ec0f0699b
barisAkgul <barisakgul1994@gmail.com>
2024-03-07 16:33:46 +0300

    Added contact page template

---
+++ src/app/(routes)/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const ContactPage = (props: Props) => {
+  return <div className="container min-h-[100vh]">ContactPage</div>;
+};
+
+export default ContactPage;
