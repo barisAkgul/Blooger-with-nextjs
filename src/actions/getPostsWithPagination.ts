eb087b7a48ff6d2b5df682516f0ad74bfe7c7da6
barisAkgul <barisakgul1994@gmail.com>
2024-03-09 21:55:36 +0300

    Added get posts with pagination

---
+++ src/actions/getPostsWithPagination.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import prismadb from "@/lib/prismadb";
+
+export async function GetPosts({
+  offset = 0,
+  limit = 20,
+}: {
+  search?: string | undefined;
+  offset?: number;
+  limit?: number;
+}) {
+  const data = await prismadb.post.findMany({
+    skip: offset,
+    take: limit,
+  });
+
+  const totalCount = await prismadb.post.count({});
+  const totalPages = Math.ceil(totalCount / limit);
+
+  return { data, totalCount, totalPages };
+}
