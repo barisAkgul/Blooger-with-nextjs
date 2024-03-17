9b09feb55c9625bb194fac3478b2b867a4b178a9
barisAkgul <barisakgul1994@gmail.com>
2024-03-18 02:20:10 +0300

    Pagination refactor

--- src/components/common/Pagination.tsx
+++ src/components/common/Pagination.tsx
@@ -6,7 +6,6 @@ import { usePathname, useSearchParams } from "next/navigation";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
 
 interface IPagination {
   totalPages: number;
@@ -15,14 +14,12 @@ interface IPagination {
 export default function Pagination({ totalPages }: IPagination) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const router = useRouter();
   const currentPage = Number(searchParams.get("page")) || 1;
 
   const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
     params.set("page", pageNumber.toString());
-
-    return `${pathname}/?${params.toString()}`;
+    return `${pathname}?${params.toString()}`;
   };
   return (
     <>
