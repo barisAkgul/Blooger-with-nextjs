8f88c7188b5c18d9ede44278fea25fb6576938b5
barisAkgul <barisakgul1994@gmail.com>
2024-03-19 01:12:40 +0300

    Use  orderBy parameter to fetch posts sorted by date in descending order.

--- src/actions/getPostsWithPagination.ts
+++ src/actions/getPostsWithPagination.ts
@@ -6,15 +6,16 @@ export async function GetPosts({
   offset = 0,
   limit = 5,
 }: {
-  search?: string | undefined;
   offset?: number;
   limit?: number;
 }) {
   const data = await prismadb.post.findMany({
     skip: offset,
     take: limit,
+    orderBy: {
+      createdAt: "desc",
+    },
   });
-
   const totalCount = await prismadb.post.count({});
   const totalPages = Math.ceil(totalCount / limit);
 
